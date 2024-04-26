5c7a3f27baaa21477c1b2a77c0ee7ba46d65bd59
Bonobotokin <vangaytech.320@yahoo.com>
2024-04-26 22:18:50 +0300

    ajustement de get anomalie photo

--- Main_Courante/api_anomalie/serializer.py
+++ Main_Courante/api_anomalie/serializer.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
 
+from Main_Courante.models import MainCourante, PhotoMC
 
-class AnomalieSerializer(serializers.ModelSerializer):
+
+class MainCouranteSerializer(serializers.ModelSerializer):
     class Meta:
-        model = 'Main_Courante'
+        model = MainCourante
+        fields = [
+            'date_mc',
+            'type_anomalie',
+            'longitude_mc',
+            'latitude_mc',
+            'description_mc',
+            'client',
+            'cp_commune',
+            'utilisateur'
+        ]
+
+
+class PhotosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhotoMC
         fields = '__all__'
--- Main_Courante/api_anomalie/views.py
+++ Main_Courante/api_anomalie/views.py
@@ -1,18 +1,32 @@
 from django.http import JsonResponse
-from rest_framework import permissions
+from rest_framework import permissions, status
+from rest_framework.decorators import parser_classes
+from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.views import APIView
 
-from Main_Courante.models import StatutMC
-
+from Main_Courante.api_anomalie.serializer import MainCouranteSerializer, PhotosSerializer
+from Main_Courante.models import StatutMC, PhotoMC
+ 
 
 class DeclareMaincourate(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def get(self, request):
+    @staticmethod
+    def get(request):
         main_courantes = StatutMC.objects.all()
 
         main_courante_list = []
         for main_courante in main_courantes:
+            photomc = PhotoMC.objects.filter(main_courante_id=main_courante.main_courante_id)
+
+            # Initialiser les attributs photo_anomalie_1 à photo_anomalie_5 à None
+            photo_attributes = {f'photo_anomalie_{i+1}': "null" for i in range(5)}
+
+            # Remplir les attributs avec les URLs des photos disponibles
+            for i, photo in enumerate(photomc[:5]):
+                if photo.photo_anomalie.url:
+                    photo_attributes[f'photo_anomalie_{i+1}'] = photo.photo_anomalie.url
+
             main_courante_info = {
                 'id': int(main_courante.main_courante_id),
                 'id_mc': int(main_courante.main_courante_id),
@@ -24,11 +38,40 @@ class DeclareMaincourate(APIView):
                 'client_declare': str(main_courante.main_courante.client.nom_client) if main_courante.main_courante.client_id else '',
                 'cp_commune': str(main_courante.main_courante.cp_commune_id) if main_courante.main_courante.cp_commune_id else '',
                 'commune': str(main_courante.main_courante.cp_commune.commune) if main_courante.main_courante.cp_commune_id else '',
-                'status': 0 if main_courante.non_traite else ( 1 if main_courante.en_cours else 2 if main_courante.realise else '')
+                'status': 0 if main_courante.non_traite else (1 if main_courante.en_cours else 2 if main_courante.realise else ''),
+                **photo_attributes,
             }
             main_courante_list.append(main_courante_info)
-            # print(main_courante_info)
-
-        # Déplacer l'impression de main_courante_info à l'intérieur de la boucle for
 
         return JsonResponse({'main_courante_list': main_courante_list})
+
+
+    @staticmethod
+    @parser_classes((MultiPartParser, FormParser))
+    def post(request):
+        maincourante_data = request.data.copy()
+        maincourante_data['utilisateur'] = request.user.id_utilisateur
+        maincourante_serializer = MainCouranteSerializer(data=maincourante_data)
+
+        if maincourante_serializer.is_valid():
+            datemc = maincourante_serializer.validated_data['date_mc']
+            main_courant = maincourante_serializer.save()
+            main_courante_id = main_courant.pk
+
+            photomc = request.data.get('photomc', [])
+            for photo in photomc:
+                photo['main_courante'] = main_courante_id
+                photo_serializer = PhotosSerializer(data=photo)
+
+                if photo_serializer.is_valid():
+                    photo_serializer.save()
+                else:
+                    return JsonResponse({'message': photo_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+            StatutMC.objects.create(
+                main_courante_id=main_courante_id,
+                date_status=datemc
+            )
+            return JsonResponse({'message': 'Données enregistrées avec succès'}, status=200)
+        else:
+            return JsonResponse({'message': maincourante_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
