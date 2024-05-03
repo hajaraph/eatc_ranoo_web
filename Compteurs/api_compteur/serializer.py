2ed9917842e3d4ee673bd608964dc94a51faf0a4
unknown <rahajarisonraphael@gmail,com>
2024-05-02 17:19:23 -0800

    amelioration de l'api

--- Compteurs/api_compteur/serializer.py
+++ Compteurs/api_compteur/serializer.py
@@ -4,29 +4,12 @@ from Compteurs.models import ReleveCompteur
 from Facturation.models import Facture, Paiement
 
 
-class ReleveCompteurSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ReleveCompteur
-        fields = ['id_releve', 'date_releve', 'volume', 'conso', 'image_compteur', 'utilisateur', 'num_compteur']
-
-
 class MissionSerializer(serializers.ModelSerializer):
     image_compteur = serializers.ImageField(required=False)  # Champ pour le fichier image_compteur
 
     class Meta:
         model = ReleveCompteur
-        fields = ['date_releve', 'volume', 'num_compteur', 'utilisateur', 'image_compteur']
-
-
-class MissionReceivePost(serializers.Serializer):
-    class Meta:
-        model = ReleveCompteur
-        fields = [
-            'num_compteur',
-            'volume_dernier_releve',
-            'date_releve',
-            'utilisateur'
-        ]
+        fields = ['id_releve', 'date_releve', 'volume', 'num_compteur', 'image_compteur']
 
 
 class FactureSerializer(serializers.ModelSerializer):
