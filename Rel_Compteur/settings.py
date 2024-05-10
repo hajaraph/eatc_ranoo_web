115ed3545b22dbbedd0311fc88d5b5f8f881cd5b
eatc <gitHub@eatc.me>
2024-05-10 05:50:19 +0000

    d

--- Rel_Compteur/settings.py
+++ Rel_Compteur/settings.py
@@ -15,7 +15,6 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django_extensions',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
