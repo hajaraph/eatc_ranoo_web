2e0ecfb31084b69a62a61dcb3ab45534f2f57415
Bonobotokin <vangaytech.320@yahoo.com>
2024-05-13 14:20:05 +0300

    correction

--- Main_Courante/api_anomalie/serializer.py
+++ Main_Courante/api_anomalie/serializer.py
@@ -3,27 +3,10 @@ from rest_framework import serializers
 from Main_Courante.models import MainCourante, PhotoMC
 
 
-<<<<<<< HEAD
 class MainCouranteSerializer(serializers.ModelSerializer):
     class Meta:
         model = MainCourante
         fields = [
-=======
-class PhotosSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PhotoMC
-        fields = '__all__'
-
-
-class MainCouranteSerializer(serializers.ModelSerializer):
-    anomalie_id = serializers.IntegerField(required=False)
-    photo_mc = PhotosSerializer(many=True, required=False)
-
-    class Meta:
-        model = MainCourante
-        fields = [
-            'anomalie_id',
->>>>>>> feature/test
             'date_mc',
             'type_anomalie',
             'longitude_mc',
@@ -31,7 +14,6 @@ class MainCouranteSerializer(serializers.ModelSerializer):
             'description_mc',
             'client',
             'cp_commune',
-<<<<<<< HEAD
             'utilisateur'
         ]
 
@@ -40,8 +22,3 @@ class PhotosSerializer(serializers.ModelSerializer):
     class Meta:
         model = PhotoMC
         fields = '__all__'
-=======
-            'utilisateur',
-            'photo_mc'
-        ]
->>>>>>> feature/test
