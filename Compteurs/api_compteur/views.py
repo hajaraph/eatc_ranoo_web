6622fe3141e0cf5e1229bfec94a07e12f075c8f7
Bonobotokin <vangaytech.320@yahoo.com>
2024-05-25 13:29:24 +0300

    change modifi statu

--- Compteurs/api_compteur/views.py
+++ Compteurs/api_compteur/views.py
@@ -200,25 +200,16 @@ class Missions(APIView):
             )
         )
 
-     date_releve = 0
-        statut = 0
-        for contrat in contrats_commune: 
-            dernier_releve = ReleveCompteur.objects.filter(num_compteur=contrat.num_compteur).order_by('date_releve').last()
-            if dernier_releve:
-                date_releve = dernier_releve.date_releve
-                if dernier_releve.date_releve.month == datetime.now().month and dernier_releve.date_releve.year == datetime.now().year:
-                    statut = 2
-                else:
-                    statut = 0
-            elif date_releve and date_releve.month != datetime.now().month:
-                statut = 0
-            else: 
-                statut = 0
+        for contrat in contrats_commune:
+            dernier_releve = ReleveCompteur.objects.filter(num_compteur=contrat.num_compteur).aggregate(
+                max_date=Max('date_releve'))
+            contrat.date_releve = dernier_releve['max_date'] if dernier_releve['max_date'] else None
 
-            # Mettre à jour le statut du contrat dans la base de données
-            contrat.statut = statut
-            contrat.save()
-
+            # Comparaison des mois pour définir le statut
+            if contrat.date_releve and contrat.date_releve.month != end_of_month.month:
+                contrat.statut = 0
+            else:
+                contrat.statut = 1
 
         liste_contrats_info = []
 
