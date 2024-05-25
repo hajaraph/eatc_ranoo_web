e2a52763e2495195e2b535e6eafc0988f54a7aa6
unknown <rahajarisonraphael@gmail,com>
2024-05-24 23:44:21 -0800

    modifier height search box

--- static/modal.js
+++ static/modal.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
             url: '/static/fr-FR.json',
         },
     });
+    $('.dataTables_filter input[type="search"]').css('height', '500px');
     // Hide automatique le message après quelque seconde
     $("#alert").animate({
         opacity: 1,
