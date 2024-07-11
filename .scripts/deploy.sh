99fba7ffbea7456c6caf1b37619615aa0aa8f243
Demondra <98756213+hajaraph@users.noreply.github.com>
2024-07-11 13:57:57 -0800

    fix post mission asyn

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 set -e
 
-echo "Deployment started ..." 
+echo "Deployment started ..."
 
 # Pull the latest version of the app
 echo "Pulling the latest changes from the main branch..."
-git pull origin main --no-rebase 
+git pull origin main --no-rebase
 echo "New changes copied to the server!"
 
 # Activate Virtual Env
 echo "Activating virtual environment 'myenv'..."
-source /home/eatc/myenv/bin/activate 
+source /home/eatc/myenv/bin/activate
 
 # Move to the project directory
+echo "Moving to the project directory..."
 cd /home/eatc/eatc_ranoo/
 
 # Install dependencies
-echo "Installing Dependencies..."
+echo "Installing dependencies..."
 pip install -r requirements.txt --no-input
 
 # Serve Static Files
-echo "Serving Static Files..."
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 # Run Database migration
-echo "Running Database migration..."
+echo "Running database migrations..."
 python manage.py makemigrations
-python manage.py migrate 
+python manage.py migrate
 
 # Deactivate Virtual Env
 echo "Deactivating virtual environment 'myenv'..."
 deactivate
 
-echo "Deployment Finished!"
-
-cd /home/eatc/eatc_ranoo/
+echo "Starting Celery worker..."
 celery -A Tasks worker --pool=solo -l info -E
+
+echo "Deployment Finished!"
