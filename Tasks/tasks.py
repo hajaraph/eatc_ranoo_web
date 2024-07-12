de9a3be5921e9abc07e6344863b7ef596b7eeb30
Demondra <98756213+hajaraph@users.noreply.github.com>
2024-07-11 21:56:11 -0800

    fix post mission asyn

--- Tasks/tasks.py
+++ Tasks/tasks.py
@@ -67,7 +67,7 @@ class TaskMission:
 
     @staticmethod
     @shared_task
-    def process_releve(data, file, id_utilisateur):
+    def process_releve(data, utilisateur):
         serializer = MissionSerializer(data=data)
 
         if not serializer.is_valid():
@@ -75,52 +75,50 @@ class TaskMission:
 
         try:
             with transaction.atomic():
-                id_releve = serializer.validated_data.get('releve_id')
                 compteur_id = serializer.validated_data.get('num_compteur')
                 date_releve = serializer.validated_data.get('date_releve')
                 volume = serializer.validated_data.get('volume')
-                image_compteur = file.get('image_compteur')
+                image_compteur = serializer.validated_data.get('image_compteur')
 
-                if id_releve is not None:
-                    compteur = get_object_or_404(Compteur, relevecompteurs__id_releve=id_releve)
-                    dernier_releve = compteur.relevecompteurs.order_by('-id_releve')[1]
+                # if id_releve is not None:
+                #     compteur = get_object_or_404(Compteur, relevecompteurs__id_releve=id_releve)
+                #     dernier_releve = compteur.relevecompteurs.order_by('-id_releve')[1]
+                #
+                #     if dernier_releve.volume >= volume:
+                #         return {'status': 'error', 'message': "Assurez-vous d'envoyer les chiffres correctement et réessayez !"}
+                #
+                #     if date_releve <= dernier_releve.date_releve:
+                #         return {'status': 'error', 'message': "Veuillez fournir une date valide"}
+                #
+                #     mod_releve = ReleveMod.mod_relever_facture(id_releve, compteur, date_releve, volume, image_compteur,
+                #                                                dernier_releve)
+                #     facture_creation(date_releve, compteur.num_compteur, mod_releve)
+                #
+                #     return {'status': 'success', 'message': 'Mise à jour effectuée avec succès !'}
+                #
+                # else:
+                if ReleveCompteur.objects.filter(num_compteur=compteur_id, date_releve=date_releve).exists():
+                    return {'status': 'error', 'message': "La date de relevé existe déjà dans la base de données"}
 
-                    if dernier_releve.volume >= volume:
-                        return {'status': 'error', 'message': "Assurez-vous d'envoyer les chiffres correctement et réessayez !"}
+                dernier_volume = ReleveCompteur.objects.filter(num_compteur=compteur_id).latest('date_releve')
 
-                    if date_releve <= dernier_releve.date_releve:
+                if dernier_volume:
+                    if date_releve <= dernier_volume.date_releve:
                         return {'status': 'error', 'message': "Veuillez fournir une date valide"}
 
-                    mod_releve = ReleveMod.mod_relever_facture(id_releve, compteur, date_releve, volume, image_compteur,
-                                                               dernier_releve)
-                    facture_creation(date_releve, compteur.num_compteur, mod_releve)
+                    if dernier_volume.volume > volume:
+                        return {'status': 'error', 'message': "Assurez-vous de saisir les chiffres correctement et "
+                                                              "réessayez !"}
 
-                    return {'status': 'success', 'message': 'Mise à jour effectuée avec succès !'}
-
+                    conso = volume - dernier_volume.volume
                 else:
-                    if ReleveCompteur.objects.filter(num_compteur=compteur_id, date_releve=date_releve).exists():
-                        return {'status': 'error', 'message': "La date de relevé existe déjà dans la base de données"}
+                    conso = volume
 
-                    dernier_volume = ReleveCompteur.objects.filter(num_compteur=compteur_id).latest('date_releve')
-
-                    if dernier_volume:
-                        if date_releve <= dernier_volume.date_releve:
-                            return {'status': 'error', 'message': "Veuillez fournir une date valide"}
-
-                        if dernier_volume.volume > volume:
-                            return {'status': 'error', 'message': "Assurez-vous de saisir les chiffres correctement et "
-                                                                  "réessayez !"}
-
-                        conso = volume - dernier_volume.volume
-                    else:
-                        conso = volume
-
-                    releve = relever(data, dernier_volume.num_compteur_id, date_releve, volume, conso,
-                                     image_compteur, id_utilisateur)
-                    facture_creation(date_releve, dernier_volume.num_compteur_id, releve)
+                releve = relever(dernier_volume.num_compteur_id, date_releve, volume, conso, image_compteur, utilisateur)
+                facture_creation(date_releve, dernier_volume.num_compteur_id, releve)
 
                 historique = f"Relever et Facture d'un compteur {compteur_id}"
-                enregistre_historique(historique, id_utilisateur)
+                enregistre_historique(historique, utilisateur)
 
                 return {'status': 'success', 'message': 'Relevé enregistré avec succès !'}
         except ValueError as e:
