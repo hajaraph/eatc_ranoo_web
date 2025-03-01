7404b044b3a1fb0d01f1abc30d85a6c4d454e126
Demondra <rahajarisonraphael@gmail.com>
2025-03-01 12:21:44 -0800

    finalisation du tenant

--- Rel_Compteur/settings.py
+++ Rel_Compteur/settings.py
@@ -120,10 +120,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django_tenants.postgresql_backend',
         'NAME': 'rel_compteur',
-        'USER': 'postgres',
-        'PASSWORD': '12121212',
-        # 'USER': 'eatcrano',
-        # 'PASSWORD': 'eatc301',
+        # 'USER': 'postgres',
+        # 'PASSWORD': '12121212',
+        'USER': 'eatcrano',
+        'PASSWORD': 'eatc301',
         'HOST': 'localhost',
         'PORT': '5432',
     }
