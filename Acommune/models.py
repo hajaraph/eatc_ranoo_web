d633466f9b6d6049278be32fa1d1374c5b33d836
Demondra <rahajarisonraphael@gmail.com>
2025-03-19 10:25:08 -0800

    ajout province

--- Acommune/models.py
+++ Acommune/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
+
+class Province(models.Model):
+    id_province = models.BigAutoField(primary_key=True)
+    province = models.CharField(max_length=50, blank=False, null=False)
+
 class Region(models.Model):
     id_region = models.BigAutoField(primary_key=True)
     region = models.CharField(max_length=20, blank=False)
+    province = models.ForeignKey(Province, on_delete=models.CASCADE, blank=False, null=False)
+
+    def __str__(self):
+        return self.province.province + " " + self.region
 
 
 class Commune(models.Model):
