ebf114102b73ae255088b9023ca10b6bd38bac1c
Demondra <rahajarisonraphael@gmail.com>
2025-06-03 12:07:56 -0800

    Mise a jour: suppression des fichiers indesirable

--- Rel_Compteur/settings.py
+++ Rel_Compteur/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # 'django_browser_reload.middleware.BrowserReloadMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    # 'django_tenants.middleware.main.TenantMainMiddleware',
+    'django_tenants.middleware.main.TenantMainMiddleware',
 ]
 
 
