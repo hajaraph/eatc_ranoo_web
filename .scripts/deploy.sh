9e57ef9af51693cedde926c4babbd9f113541c1e
unknown <rahajarisonraphael@gmail.com>
2026-02-14 09:31:29 +0300

    Mise à jour : correction filtre

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -20,7 +20,7 @@ cd /home/eatc/eatc_ranoo/
 
 # Installation des dépendances
 echo "📦 Installation des dépendances..."
-pip install --no-deps -r requirements.txt --no-input
+pip install -r requirements.txt --no-input
 
 # Définition des permissions d'exécution pour gunicorn_start.sh
 echo "🔒 Définition des permissions d'exécution pour gunicorn_start.sh..."
